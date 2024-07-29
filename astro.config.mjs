d5abeabb74f6826d497e6efbda50101ce5ab7a38
ChrisDTG <chrisdtg2001@gmail.com>
2024-07-29 20:10:32 -0300

    Components re-use, css recycle, fixed, etc

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'static'
+    output: 'static',
+    devToolbar: {
+      enabled: false
+    }
 });
 // astro check && 
